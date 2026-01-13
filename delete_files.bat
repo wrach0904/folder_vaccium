075cb11cc0a356431186cfc621507a40360f721e
wrach0904 <wiktoria.r.chmielewska@gmail.com>
2026-01-13 13:33:40 +0100

    poprawki

---
+++ delete_files.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd /d "C:\Users\damne\OneDrive\Documents\PyCharm\PythonPROGRAMS\folder_vaccium"
+
+start "" "C:\Users\damne\OneDrive\Documents\PyCharm\PythonPROGRAMS\folder_vaccium\venv\Scripts\pythonw.exe" "main.pyw"
+
+exit
